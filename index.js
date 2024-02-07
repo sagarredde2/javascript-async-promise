550ce4bb3ad0da35e4e9006aef13b27c760016f8
Sagar Reddy <80266466+sagarredde2@users.noreply.github.com>
2024-02-07 08:47:48 +0530

    Promise code

--- index.js
+++ index.js
@@ -25,5 +25,36 @@ asyncProm();
 
 
 
+const getData = fetch("https://api.github.com/users/sagarredde2");
+console.log(getData);
+getData.then(
+  function res(data) {
+    console.log(data.json());
+  },
+  function rej(error) {
+    console.log(error);
+  },
+);
 
+function makeApiCall(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
+}
+
+// Example usage:
+const apiUrl = "https://api.github.com/users/sagarredde2";
+
+makeApiCall(apiUrl)
+  .then((responseData) => {
+    console.log("API call successful:", responseData);
+  })
+  .catch((error) => {
+    console.error("API call failed:", error);
+  });
 
